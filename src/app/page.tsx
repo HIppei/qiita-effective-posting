0b6a2221b3a57b6e1bd1e2c26fef42f0aaab0792
HIppei <ip.horiba@gmail.com>
2024-07-16 22:25:08 +0900

    Revert test for eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,8 +40,6 @@ export type QiitaArticle = {
   stocks_count: number;
 };
 
-const a = '';
-
 export default function Home() {
   const { userInfo } = useContext(UserInfoContext);
   const [data, setData] = useState<Article[] | undefined>();
